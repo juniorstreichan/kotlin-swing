bb4e729918fa29538e86bd691bd61077ab5a4ac0
Junior Erdmann Streichan <juniorstreichan@gmail.com>
2018-06-25 12:57:57 -0400

    Apresentação dos detalhes

--- src/main/kotlin/ui/PostDetail.java
+++ src/main/kotlin/ui/PostDetail.java
@@ -1,21 +1,32 @@
 package ui;
 
+import business.PostBusiness;
+import entity.PostEntity;
+
 import javax.swing.*;
 
 public class PostDetail extends JDialog {
     private JPanel rootPanel;
     private JLabel lbTitulo;
     private JLabel lbDescricao;
-    private JButton buttonOK;
+
+    private PostBusiness business = new PostBusiness();
 
     public PostDetail(int id) {
         super();
-
+        loadPost(id);
+        setModal(true);
         this.setContentPane(rootPanel);
         setSize(400, 400);
         setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
         setVisible(true);
 
+    }
+
+    private void loadPost(int id) {
+        PostEntity entity = business.getSinglePosts(id);
+        lbTitulo.setText(entity.getTitle());
+        lbDescricao.setText("<html>"+entity.getBody()+"</html>");
     }
 
 }
